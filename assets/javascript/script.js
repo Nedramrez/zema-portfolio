e836c1ef23c5486b09ba3b54182bc74816581905
Nedramrez <zermarden@gmail.com>
2022-09-08 16:18:38 +0600

    Fix the close icon and make links work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 const appearMobileMenu = document.querySelector('.mobile-menu');
 const disAppearMobileMenu = document.querySelector('.mobile-menu');
+const disAppearMobileMenuPortfolio = document.querySelector('.mobile-menu');
+const disAppearMobileMenuAbout = document.querySelector('.mobile-menu');
+const disAppearMobileMenuContact = document.querySelector('.mobile-menu');
 
 function mobileMenu() {
   const appearMobileDivision = document.createElement('div');
@@ -31,6 +34,7 @@ function mobileMenu() {
   appearMobileUlLi1.appendChild(appearMobileLink1);
   appearMobileLink1.setAttribute('href', '#Portfolio');
   appearMobileLink1.textContent = 'Portfolio';
+  appearMobileLink1.setAttribute('onclick', 'disappearMobileMenuPortfolio()');
 
   const appearMobileUlLi2 = document.createElement('li');
   appearMobileUlLi2.classList.add('mobile-list-item');
@@ -41,6 +45,7 @@ function mobileMenu() {
   appearMobileUlLi2.appendChild(appearMobileLink2);
   appearMobileLink2.setAttribute('href', '#About');
   appearMobileLink2.textContent = 'About';
+  appearMobileLink1.setAttribute('onclick', 'disappearMobileMenuAbout()');
 
   const appearMobileUlLi3 = document.createElement('li');
   appearMobileUlLi3.classList.add('mobile-list-item');
@@ -51,6 +56,7 @@ function mobileMenu() {
   appearMobileUlLi3.appendChild(appearMobileLink3);
   appearMobileLink3.setAttribute('href', '#Contact');
   appearMobileLink3.textContent = 'Contact';
+  appearMobileLink1.setAttribute('onclick', 'disappearMobileMenuContact()');
 
   const MobileMenu = document.getElementById('id-for-js');
   MobileMenu.style.display = 'block';
@@ -67,4 +73,22 @@ function disappearMobileMenu() {
   removeMobileMenu.remove();
 }
 
+function disappearMobileMenuPortfolio() {
+  disappearMobileMenu();
+  window.open('../#contact', '_self');
+}
+
+function disappearMobileMenuAbout() {
+  disappearMobileMenu();
+  window.open('../#about', '_self');
+}
+
+function disappearMobileMenuContact() {
+  disappearMobileMenu();
+  window.open('../#portfolio', '_self');
+}
+
 disAppearMobileMenu.addEventListener('click', disappearMobileMenu);
+disAppearMobileMenuPortfolio.addEventListener('click', disappearMobileMenuPortfolio);
+disAppearMobileMenuAbout.addEventListener('click', disappearMobileMenuAbout);
+disAppearMobileMenuContact.addEventListener('click', disappearMobileMenuContact);
