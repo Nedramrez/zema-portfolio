6084c5bab1ad11e18768041a6647523f3c2106da
Nedramrez <zermarden@gmail.com>
2022-09-14 14:27:46 +0600

    Add popup window code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -101,28 +101,28 @@ const works = [
     image: 'assets/images/d-snapshot-3.svg',
     see: 'https://nedramrez.github.io/portfolio',
     repo: 'https://github.com/nedramrez/portfolio',
-    order: 'order-0',
+    order: 0,
   },
   {
     title: 'Multi-post stories',
     image: 'assets/images/snapshoot-portfolio.svg',
     see: 'https://nedramrez.github.io/portfolio',
     repo: 'https://github.com/nedramrez/portfolio',
-    order: 'order-1',
+    order: 1,
   },
   {
     title: 'Tonic',
     image: 'assets/images/snapshot-card-3.svg',
     see: 'https://nedramrez.github.io/portfolio',
     repo: 'https://github.com/nedramrez/portfolio',
-    order: 'order-2',
+    order: 2,
   },
   {
     title: 'Multi-post stories',
     image: 'assets/images/snapshot-card-4.svg',
     see: 'https://nedramrez.github.io/portfolio',
     repo: 'https://github.com/nedramrez/portfolio',
-    order: 'order-3',
+    order: 3,
   },
 ];
 
@@ -140,20 +140,83 @@ const tags = {
   javascript: 'javascript',
 };
 
-// eslint-disable-next-line no-unused-vars
-const detailsPp = function detailsPp() {
-  const x = document.getElementById('pp-details');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
+function detailsPp() {
+  const orderNum = works.order;
+  for (let i = 0; i < works.length; i += 1) {
+    if (i === orderNum) {
+      works.includes(orderNum);
+      const portfolio = document.querySelector('#portfolio');
+      const article = document.createElement('article');
+      article.classList.add(works.includes(orderNum).order);
+      portfolio.appendChild(article);
+      const snapshot = document.createElement('img');
+      article.appendChild(snapshot);
+      snapshot.classList.add('project-img');
+      snapshot.setAttribute('src', works.image);
+      const prDetails = document.createElement('div');
+      article.appendChild(prDetails);
+      prDetails.classList.add('pr-details');
+      const title = document.createElement('h3');
+      prDetails.appendChild(title);
+      title.innerText = works.title;
+      const listDetails = document.createElement('ul');
+      prDetails.appendChild(listDetails);
+      const lineDetail1 = document.createElement('li');
+      listDetails.appendChild(lineDetail1);
+      const client = document.createElement('h5');
+      client.innerText = details.client;
+      lineDetail1.appendChild(client);
+      const lineDetail2 = document.createElement('li');
+      listDetails.appendChild(lineDetail2);
+      const counter = document.createElement('img');
+      counter.setAttribute('src', details.counter);
+      counter.setAttribute('class', 'counter');
+      counter.setAttribute('alt', 'counter');
+      lineDetail2.appendChild(counter);
+      const lineDetail3 = document.createElement('li');
+      listDetails.appendChild(lineDetail3);
+      const role = document.createElement('h6');
+      role.innerText = details.role;
+      lineDetail3.appendChild(role);
+      const lineDetail4 = document.createElement('li');
+      listDetails.appendChild(lineDetail4);
+      const counter2 = document.createElement('img');
+      counter2.setAttribute('src', details.counter);
+      counter.setAttribute('class', 'counter');
+      counter.setAttribute('alt', 'counter');
+      lineDetail4.appendChild(counter2);
+      const lineDetail5 = document.createElement('li');
+      listDetails.appendChild(lineDetail5);
+      const year = document.createElement('h6');
+      year.innerText = details.year;
+      lineDetail5.appendChild(year);
+      const description = document.createElement('p');
+      description.innerText = details.description;
+      prDetails.appendChild(description);
+      const tagsList = document.createElement('ul');
+      tagsList.classList.add('tags');
+      prDetails.appendChild(tagsList);
+      const lineTag1 = document.createElement('li');
+      lineTag1.innerText = tags.html;
+      tagsList.appendChild(lineTag1);
+      const lineTag2 = document.createElement('li');
+      lineTag2.innerText = tags.css;
+      tagsList.appendChild(lineTag2);
+      const lineTag3 = document.createElement('li');
+      lineTag3.innerText = tags.javascript;
+      tagsList.appendChild(lineTag3);
+      const button = document.createElement('button');
+      button.innerText = 'See Project';
+      button.setAttribute('id', 'see-project');
+      button.setAttribute('onclick', 'detailsPp()');
+    }
   }
-};
+}
 
 works.forEach(work => { // eslint-disable-line
   const portfolio = document.querySelector('#portfolio');
   const article = document.createElement('article');
-  article.classList.add(work.order);
+  article.classList.add(`order-${work.order}`);
   portfolio.appendChild(article);
   const snapshot = document.createElement('img');
   article.appendChild(snapshot);
@@ -213,6 +276,9 @@ works.forEach(work => { // eslint-disable-line
   tagsList.appendChild(lineTag3);
   const button = document.createElement('button');
   button.innerText = 'See Project';
-  button.setAttribute('onclick', 'detailsPp()');
+  button.setAttribute('id', 'see-project');
   prDetails.appendChild(button);
 });
+
+const seeProject = document.querySelector('#see-project');
+seeProject.addEventListener('click', detailsPp);
